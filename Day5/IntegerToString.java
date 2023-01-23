468004f43d635294bc042d12e6ef2b936d8369f3
Shouvik <deshouvik078@gmail.com>
2023-01-23 09:55:25 +0530

    new

---
+++ Day5/IntegerToString.java
@@ -0,0 +1,11 @@
+// Integer to String
+
+package Day5;
+
+public class IntegerToString {
+    public static void main(String[] args) {
+        int num = 88;
+        String str = String.valueOf(num);
+        System.out.println(str);
+    }
+}
---
+++ Day5/StringtoInt.java
@@ -0,0 +1,11 @@
+// String to Integer
+
+package Day5;
+
+public class StringtoInt {
+    public static void main(String[] args) {
+        String str = "38";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+    }
+}
