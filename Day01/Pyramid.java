784ae09d395dbe35ccb807797bf7492310361d1d
Shouvik <deshouvik078@gmail.com>
2023-01-30 18:11:46 +0530

    Revision

---
+++ Day01/Pyramid.java
@@ -0,0 +1,18 @@
+package Day01;
+
+public class Pyramid
+
+{
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
