a801a469e71a6e9bbb263b34dc2eade86157181c
Shouvik <deshouvik078@gmail.com>
2023-03-15 16:25:21 +0530

    Day53

---
+++ Day53/PreviousSmallerElement.java
@@ -0,0 +1,31 @@
+package Day53;
+
+import java.util.Stack;
+
+public class PreviousSmallerElement {
+    public static void main(String[] args) {
+        int[] arr = { 8, 2, 6, 5, 4, 7 };
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (st.size() > 0 && arr[i] < arr[st.peek()]) {
+                ans[st.peek()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        // element are left whoes previous element is not found
+        while (st.size() > 0) {
+            ans[st.peek()] = -1;
+            st.pop();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
